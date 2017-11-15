f7b74e1c07dd22a5fc3e95bd1d7c3ab9c9c7fa10
lupyuen-unabiz <ly.lee@unabiz.com>
2017-11-15 18:53:39 +0800

    Updated dependencies

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 //  Environment Variables:
 //  NODE_ENV=production
 //  AUTOINSTALL_DEPENDENCY= sigfox-aws-data
-//  AUTOINSTALL_VERSION= >=0.0.3
+//  AUTOINSTALL_VERSION= >=0.0.4
 //  sigfox_dbclient=mysql
 //  sigfox_dbhost=mysql host address
 //  sigfox_dbuser=mysql user
